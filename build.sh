0d38098a293c5e0dbcaf80eabbf69d363bd6927b
fajar4561 <maulanafajar751@gmail.com>
2021-07-09 08:11:00 +0700

    uodate

--- build.sh
+++ build.sh
@@ -50,21 +50,21 @@ DEVICE="X00TD"
 
 # The defconfig which should be used. Get it from config.gz from
 # your device or check source
-DEFCONFIG=signature_defconfig
+DEFCONFIG=X00TD_defconfig
 
 # Show manufacturer info
 MANUFACTURERINFO="ASUSTek Computer Inc."
 
 # Kernel Variant
 NAMA=Signature
-JENIS=EAS
-VARIAN=OC
+JENIS=EAS-Stock
+VARIAN=STOCK
 # Build Type
 BUILD_TYPE="Nightly"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc'
-COMPILER=clang
+COMPILER=gcc
 
 # Kernel is LTO
 LTO=0
