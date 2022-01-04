d64d7490928fd98dc01940d9d32fbe6d407d9b3f
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2022-01-05 06:51:22 +0700

    gcc

--- build.sh
+++ build.sh
@@ -55,9 +55,9 @@ DEFCONFIG=wizard_defconfig
 MANUFACTURERINFO="ASUSTek Computer Inc."
 
 # Kernel Variant
-NAMA=[EAS]Electro
+NAMA=[HMP]Electro
 JENIS=Wizard
-VARIAN=Zeppelin
+VARIAN=CAF
 TYPE=NLV
 
 # Build Type
@@ -65,7 +65,7 @@ BUILD_TYPE="Nightly"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49'
-COMPILER=clang
+COMPILER=gcc
 
 # Message on anykernel when installation
 MESSAGE="don't blame me if u get poor battery backup or weak performance . i'm not responsible . Do with Your Own Risk."
