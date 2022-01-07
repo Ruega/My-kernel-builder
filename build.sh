f039cc58c302276eb755cecd475d8bfe229d1a96
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2022-01-07 15:39:47 +0700

    1539

--- build.sh
+++ build.sh
@@ -57,7 +57,7 @@ MANUFACTURERINFO="ASUSTek Computer Inc."
 # Kernel Variant
 NAMA=Electro
 JENIS=Wizard
-VARIAN=NLV
+VARIAN=BOTH
 ANU=HMP
 
 # Build Type
@@ -197,7 +197,7 @@ setversioning() {
 exports() {
 	export KBUILD_BUILD_USER="queen"
 	export KBUILD_BUILD_HOST="18ded16aaef9"
-	export KBUILD_BUILD_VERSION="1"
+	export KBUILD_BUILD_VERSION="1539"
 	export ARCH=arm64
 	export SUBARCH=arm64
 
