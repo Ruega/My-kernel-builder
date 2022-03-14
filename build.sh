86f28ba4fd863562df20dca791c9574b0b8af675
T.B.S <86312158+Tiktodz@users.noreply.github.com>
2022-03-14 16:02:39 +0700

    change tc

--- build.sh
+++ build.sh
@@ -141,7 +141,7 @@ DATE2=$(TZ=Asia/Jakarta date +"%Y%m%d")
 	if [ $COMPILER = "clang" ]
 	then
 		msg "|| Cloning toolchain ||"
-		git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master clang
+		git clone --depth=1 https://gitlab.com/LeCmnGend/proton-clang -b clang-15 clang
 
 	elif [ $COMPILER = "gcc49" ]
 	then
