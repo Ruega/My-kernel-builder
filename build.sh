d67d9465dd64896c54ff275e97896ebb840e551a
T.B.S <86312158+Tiktodz@users.noreply.github.com>
2022-05-16 23:32:17 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -57,8 +57,8 @@ MANUFACTURERINFO="ASUSTek Computer Inc."
 # Kernel Variant
 NAMA=EW
 JENIS=Siren
-VARIAN=EAS
-ANU=TPD
+VARIAN=HMP
+ANU=CAF
 
 # Build Type
 BUILD_TYPE="Nightly"
@@ -141,7 +141,7 @@ DATE2=$(TZ=Asia/Jakarta date +"%Y%m%d")
 	if [ $COMPILER = "clang" ]
 	then
 		msg "|| Cloning toolchain ||"
-		git clone --depth=1 https://gitlab.com/LeCmnGend/proton-clang -b clang-15 clang
+		git clone --depth=1 https://github.com/Rasenkai/proton-clang -b master clang
 
 	elif [ $COMPILER = "gcc49" ]
 	then
@@ -196,8 +196,7 @@ setversioning() {
 
 exports() {
 	export KBUILD_BUILD_USER="queen"
-	export KBUILD_BUILD_HOST="18ded16aaef9"
-	export KBUILD_BUILD_VERSION="2"
+	export KBUILD_BUILD_VERSION="1"
 	export ARCH=arm64
 	export SUBARCH=arm64
 
